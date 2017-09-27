bdfadda0ab6251e09bec50152451f53863761e5f
softcon17 <32325449+softcon17@users.noreply.github.com>
2017-09-27 12:17:42 +0100

    add database creation and population scripts

---
+++ populate_the_database.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.MACHINES (id, name, status, location) VALUES ('947ecd65-5997-4919-b5e0-daeb6f969496','Time Travelling Machine','Online', 'LAB 1');
+INSERT INTO public.MACHINES (id, name, status, location) VALUES ('b27a9c93-6c2a-48d3-9693-ece0b93818a6','Bitcoin Miner 001','Offline', 'LAB 1');
+INSERT INTO public.MACHINES (id, name, status, location) VALUES ('a6fb1c0c-8fc5-4473-8da0-7b68f9235ea8','Poker Bot','Disabled', 'LAB 1');
+INSERT INTO public.MACHINES (id, name, status, location) VALUES ('55c547da-66e8-40f6-8807-bfde9a708206','Robot Hoover','Online', 'LAB 2');
+INSERT INTO public.MACHINES (id, name, status, location) VALUES ('52f72699-c684-4986-b2e2-13b46589baf2','Cat MEME Generator','Online', 'LAB 2');
